b78321b7abbbeb8c4b2f4c748a7e9ec87e1098e1
Vaishnav Gadia <vaishnavgadia2000@gmail.com>
2018-09-25 18:49:59 +0530

    hello,android developer

---
+++ vg.java
@@ -0,0 +1,54 @@
+/*class vg
+{
+	public stat	int x=5, y=2;
+	double z=Math.pow(x,y);
+	System.out.println(+z);
+}
+}
+
+
+class vg
+{
+	public static void main(String[] args) {
+		{
+			int j=1, i;
+			for(i=1;i<=5;i++)
+			{
+				 j=j*i;
+			}
+			System.out.println(j);
+		}
+	}
+}
+
+class vg
+{
+	public static void main(String[] args) {
+		
+	}
+}
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	int T,N=0;
+	printf("Enter the total no. you want to insert");
+	scanf("%d", &T);
+	for(T=0,T<=T-1;T++)
+		scanf("%d", &N);
+	for (n=9;n>=0;n--) {
+		while(N%(10^n)!=0)
+			
+	}
+	
+*/
+	class hello
+	{
+	public static void main(String []args) {
+System.out.println("Hello World");
+}
+	}
